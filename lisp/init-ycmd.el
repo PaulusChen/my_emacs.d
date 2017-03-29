d270ca4d0810d09ca19be18f8b95b6b7c7dff6ba
chenpeng <chenpeng@voole.com>
2017-03-29 18:52:11 +0800

    add ycmd

--- lisp/init-ycmd.el
+++ lisp/init-ycmd.el
@@ -5,8 +5,8 @@
 (add-hook 'c++-mode-hook 'ycmd-mode)
 ;;路径就是你自己的ycmd的路径
 (set-variable 'ycmd-server-command
-              '("python" "~/.emacs.d/ycmd/ycmd"))
-(set-variable 'ycmd-global-config "/home/.ycm_extra_conf.py")
+              '("python" "~/ycmd/ycmd"))
+(set-variable 'ycmd-global-config "~/.ycm_extra_conf.py")
 
 (require 'company-ycmd)
 (company-ycmd-setup)
